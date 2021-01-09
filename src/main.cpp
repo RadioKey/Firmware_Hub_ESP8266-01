e2b8df04a5a673b6437e8b64318e81adc396e53c
Dmytro Sokil <dmytro.sokil@gmail.com>
2021-01-09 02:08:35 +0200

    configure pubsub

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,62 @@
+#include <secrets.h>
 #include <RCSwitch.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiManager.h>
-#include <secrets.h>
+#include <ArduinoJson.h>
 
 int TRANSMITTER_PIN = 2;
-
-char* commandsMqttTopicName = SECRET_MQTT_BROCKER_COMMANDS_TOPIC;
 
 // See https://github.com/sui77/rc-switch/ for details
 RCSwitch switcher = RCSwitch();
 
 // See https://github.com/knolleary/pubsubclient
 PubSubClient mqttClient;
+
+String MACAddress;
+
+const char* commandsMqttTopic;
+
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, payload);
+
+  if (topic == commandsMqttTopic) {
+    String command = doc["command"];
+
+    if (command == "send") {
+      // send received code to switcher
+      int protocol = doc["protocol"];
+      int repeats = doc["repeats"];
+      int pulseLength = doc["pulseLength"];
+      const char* code = doc["code"];
+
+      switcher.setProtocol(protocol);
+      switcher.setPulseLength(pulseLength);
+      switcher.setRepeatTransmit(repeats);
+      switcher.send(code);
+    }
+  }
+
+  // sleep for next command read
+  delay(20000);
+}
+
+void reconnectMqttClient()
+{
+// Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    String clientId = "RadioKeyHub_" + MACAddress;
+
+    // Attempt to connect
+    if (mqttClient.connect(clientId.c_str())) {
+      mqttClient.subscribe(commandsMqttTopic);
+    } else {
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -29,12 +73,16 @@ void setup() {
     SECRET_WIFI_AUTOCONFIGURE_PASSWORD
   );
 
+  MACAddress = WiFi.macAddress();
+
   // MQTT client
   mqttClient.setServer(
     SECRET_MQTT_BROCKER_HOST, 
     SECRET_MQTT_BROCKER_PORT
   );
   mqttClient.setCallback(mqttCallback);
+
+  commandsMqttTopic = MACAddress.c_str();
 }
 
 void loop() {  
@@ -43,32 +91,4 @@ void loop() {
   }
 
   mqttClient.loop();
-}
-
-void mqttCallback(char* topic, byte* payload, unsigned int length)
-{
-  // send received code to switcher
-  switcher.setProtocol(1);
-  switcher.setPulseLength(309);
-  switcher.setRepeatTransmit(25);
-  switcher.send("110111101110011000110001"); // first button
-  switcher.send("110111101110011000110100"); // second button
-
-  // sleep for next command read
-  delay(20000);
-}
-
-void reconnectMqttClient()
-{
-// Loop until we're reconnected
-  while (!mqttClient.connected()) {
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (mqttClient.connect(clientId.c_str())) {
-      mqttClient.subscribe(SECRET_MQTT_BROCKER_TOPIC);
-    } else {
-      delay(5000);
-    }
-  }
 }
