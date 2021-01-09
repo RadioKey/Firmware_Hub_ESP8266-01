6fafba30f05be4cf73587aa3eda6e867cbc8c1aa
Dmytro Sokil <dmytro.sokil@gmail.com>
2021-01-09 10:18:33 +0200

    configure mqtt

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 * CH_PD has to be HIGH
 * GPIO0 has to be LOW during boot (can be HIGH once in bootloader mode)
 * You can provoque a reset of the card by a rising edge on the RESET pin.
+
+## MQTT Server
+
+Use following server for test purposes
+
+./bin/artemis create radiokey --require-login --user test --password test
+./radiokey/bin/artemis run - start broker
+./radiokey/bin/artemis-service start - start broker in background
+
+HTTP Server started at http://localhost:8161
+Artemis Jolokia REST API available at http://localhost:8161/console/jolokia
+Artemis Console available at http://localhost:8161/console
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
-#include <secrets.h>
+#include <FS.h>
 #include <RCSwitch.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
 
+/**
+ * This access point credentials used to start hub in access point mode 
+ * and configure connection of hab as station to access point
+ */
+#define SECRET_WIFI_AUTOCONFIGURE_SSID "RadioKey"
+#define SECRET_WIFI_AUTOCONFIGURE_PASSWORD "zGatQd12la"
+
 int TRANSMITTER_PIN = 2;
+char* configFilePath = "/config.json";
+bool shouldSaveConfiguration = false;
 
 // See https://github.com/sui77/rc-switch/ for details
 RCSwitch switcher = RCSwitch();
@@ -13,9 +22,39 @@ RCSwitch switcher = RCSwitch();
 // See https://github.com/knolleary/pubsubclient
 PubSubClient mqttClient;
 
+char mqttHost[40];
+char mqttPort[6]  = "8161";
+char mqttUser[40];
+char mqttPassword[40];
+
 String MACAddress;
 
 const char* commandsMqttTopic;
+
+void loadConfiguration() 
+{
+  if (SPIFFS.begin()) {
+    if (SPIFFS.exists(configFilePath)) {
+      File configFile = SPIFFS.open(configFilePath, "r");
+      if (configFile) {
+        size_t size = configFile.size();
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonDocument json(1024);
+        deserializeJson(json, buf.get());
+
+        strcpy(mqttHost, json["mqttHost"]);
+        strcpy(mqttPort, json["mqttPort"]);
+        strcpy(mqttUser, json["mqttUser"]);
+        strcpy(mqttPassword, json["mqttPassword"]);
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+}
 
 void mqttCallback(char* topic, byte* payload, unsigned int length)
 {
@@ -43,19 +82,24 @@ void mqttCallback(char* topic, byte* payload, unsigned int length)
   delay(20000);
 }
 
-void reconnectMqttClient()
+void reconnectMqttClient(char* user, char* password)
 {
 // Loop until we're reconnected
   while (!mqttClient.connected()) {
     String clientId = "RadioKeyHub_" + MACAddress;
 
     // Attempt to connect
-    if (mqttClient.connect(clientId.c_str())) {
+    if (mqttClient.connect(clientId.c_str(), user, password)) {
       mqttClient.subscribe(commandsMqttTopic);
     } else {
       delay(5000);
     }
   }
+}
+
+void wifiManagerSaveConfigCallback()
+{
+  shouldSaveConfiguration = true;
 }
 
 void setup() {
@@ -68,17 +112,55 @@ void setup() {
   WiFiManager wifiManager;
   wifiManager.setDebugOutput(false);
   wifiManager.setConfigPortalTimeout(30);
+  wifiManager.setSaveConfigCallback(wifiManagerSaveConfigCallback);
+
+  // Configure custom params during wifi management
+  WiFiManagerParameter mqttHostParameter("mqtt_host", "MQTT host", mqttPort, 40);
+  wifiManager.addParameter(&mqttHostParameter);
+
+  WiFiManagerParameter mqttPortParameter("mqtt_port", "MQTT port", mqttPort, 6);
+  wifiManager.addParameter(&mqttPortParameter);
+
+  WiFiManagerParameter mqttUserParameter("mqtt_user", "MQTT user", mqttUser, 40);
+  wifiManager.addParameter(&mqttUserParameter);
+
+  WiFiManagerParameter mqttPasswordParameter("mqtt_password", "MQTT password", mqttPassword, 40);
+  wifiManager.addParameter(&mqttPasswordParameter);
+
+  // connect wifi
   wifiManager.autoConnect(
     SECRET_WIFI_AUTOCONFIGURE_SSID, 
     SECRET_WIFI_AUTOCONFIGURE_PASSWORD
   );
 
+  // renew params from config
+  strcpy(mqttHost, mqttHostParameter.getValue());
+  strcpy(mqttPort, mqttPortParameter.getValue());
+  strcpy(mqttUser, mqttUserParameter.getValue());
+  strcpy(mqttPassword, mqttPasswordParameter.getValue());
+
+  if (shouldSaveConfiguration) {
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqttHost"] = mqttHost;
+    json["mqttPort"] = mqttPort;
+    json["mqttUser"] = mqttUser;
+    json["mqttPassword"] = mqttPassword;
+
+    File configFile = SPIFFS.open(configFilePath, "w");
+
+    json.printTo(configFile);
+    configFile.close();
+
+    shouldSaveConfiguration = false;
+  }
+
   MACAddress = WiFi.macAddress();
 
   // MQTT client
   mqttClient.setServer(
-    SECRET_MQTT_BROCKER_HOST, 
-    SECRET_MQTT_BROCKER_PORT
+    mqttHost, 
+    mqttPort
   );
   mqttClient.setCallback(mqttCallback);
 
@@ -87,7 +169,7 @@ void setup() {
 
 void loop() {  
   if (!mqttClient.connected()) {
-    reconnectMqttClient();
+    reconnectMqttClient(mqttUser, mqttPassword);
   }
 
   mqttClient.loop();
